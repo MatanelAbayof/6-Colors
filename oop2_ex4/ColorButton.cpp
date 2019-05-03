21928e77339604a26ff4967d53342a72fbae16dc
נחום נגה <nahumnoga@gmail.com>
2019-05-03 17:46:55 +0300

    GameMenu ColorPanel

---
+++ oop2_ex4/ColorButton.cpp
@@ -0,0 +1,23 @@
+#include "ColorButton.h"
+
+
+ColorButton::ColorButton(sf::RenderWindow& window, const sf::Color color)
+	: Button(window)
+{
+	setColor(color);
+}
+
+sf::Color ColorButton::getColor() const
+{
+	return m_color;
+}
+
+void ColorButton::setColor(const sf::Color color)
+{
+	m_color = color;
+}
+
+string ColorButton::toString() const
+{
+	return "ColorButton: " + Button::toString();
+}
