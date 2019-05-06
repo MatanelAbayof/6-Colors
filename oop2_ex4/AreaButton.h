366dc8057d7084dc4e99e8d1b51928168dab6048
נחום נגה <nahumnoga@gmail.com>
2019-05-06 18:31:21 +0300

    area

---
+++ oop2_ex4/AreaButton.h
@@ -0,0 +1,28 @@
+#pragma once
+//---- include section ------
+#include <string>
+#include "TextView.h"
+//---- using section --------
+using std::string;
+
+/*
+ * AreaButtun class
+ */
+class AreaButton :
+	public GUI::TextView
+{
+public:
+	// constructor
+	AreaButton(sf::RenderWindow& window, const string& text = "", float areaPercent = 0);
+	//get area percent
+	float& getAreaPercent();
+	//set areaPercent
+	void setAreaPercent(float area);
+	// convert to string
+	virtual string toString() const;
+private:
+	// quantity of area in percent
+	float m_areaPercent;
+};
+
+
