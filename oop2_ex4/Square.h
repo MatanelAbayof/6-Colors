3509d8bd1b3255ab8b92be450de88182928a17f7
נחום נגה <nahumnoga@gmail.com>
2019-05-07 13:13:17 +0300

    square

--- oop2_ex4/Square.h
+++ oop2_ex4/Square.h
@@ -1,7 +1,7 @@
 #pragma once
 //---- include section ------
 #include <string>
-#include "Shape.h"
+#include "PolygonShape.h"
 //---- using section --------
 using std::string;
 
@@ -9,18 +9,23 @@ using std::string;
  * Square class
  */
 class Square :
-	public Shape
+	public PolygonShape
 {
 public:
 	const int NUM_OF_EDGES = 4;
 	// constructor
-	Square(float width, const sf::Color& color);
+	Square(const sf::Color& color);
+	//get width
+	virtual float getWidth() const { return getHeight(); }
+	//get height
+	virtual float getHeight() const { return (getPoint(2).y - getPoint(0).y); }
 	//num of edges
-	virtual int getNumOfEdges() const;
+	virtual int getNumOfEdges() const { return NUM_OF_EDGES; }
 	// convert to string
 	virtual string toString() const;
 private:
-	// TODO members here
+	//init 
+	void init();
 };
 
 
