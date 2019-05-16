0e1b6d02d16348a88da76f394e3aee41ed953a11
נחום נגה <nahumnoga@gmail.com>
2019-05-16 12:13:50 +0300

    multi

--- oop2_ex4/GameController.h
+++ oop2_ex4/GameController.h
@@ -26,7 +26,7 @@ private:
 	// run chose mode AI screen
 	void runChooseModeAIScreen(sf::RenderWindow& window);
 	// run game screen
-	void runGameScreen(sf::RenderWindow& window, std::vector<std::shared_ptr<PlayerBase>>& players);
+	void runGameScreen(sf::RenderWindow& window, std::vector<std::shared_ptr<PlayerBase>>& players, bool userPlayFirst = true);
 	// create game
 	void createGame(GameScreen& gameScreen, std::vector<std::shared_ptr<PlayerBase>>& players, bool userStartbottomLeftVertex = true);
 	// play game
